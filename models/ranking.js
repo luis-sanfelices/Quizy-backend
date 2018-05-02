5c9a8ae5ad8d63d9d9cb371c8e9d90ddc5bd729c
luis-sanfelices <l.fernando.sanfelices@gmail.com>
2018-05-02 08:38:33 +0200

    las changes

--- models/ranking.js
+++ models/ranking.js
@@ -2,24 +2,29 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const rankingSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-    index: true,
+const rankingSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      index: true,
+    },
+    quizId: {
+      type: Schema.Types.ObjectId,
+      ref: 'quiz',
+      index: true,
+    },
+    category: {
+      type: String,
+      index: true,
+    },
+    result: { type: Number, default: 0 },
+    date: Date,
   },
-  quizId: {
-    type: Schema.Types.ObjectId,
-    ref: 'quiz',
-    index: true,
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
   },
-  category: {
-    type: String,
-    index: true,
-  },
-  result: { type: Number, default: 0 },
-  date: Date,
-});
+);
 
 
 const Ranking = mongoose.model('ranking', rankingSchema);
