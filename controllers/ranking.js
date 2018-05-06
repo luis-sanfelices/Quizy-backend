52a0c0d8c12295af84f3f222362102725ed9ce6e
julia <juliabellone@gmail.com>
2018-05-06 11:22:08 +0200

    endopoint ranking

--- controllers/ranking.js
+++ controllers/ranking.js
@@ -6,6 +6,18 @@ const rankingController = {
       .then(ranking => res.status(200).json(ranking))
       .catch(err => next(err));
   },
+  getRanking(req, res, next) {
+    const {quizId} = req.query; 
+    Ranking.find( {quizId} )
+      .sort({result: -1, date: -1})
+      .limit(10)
+      .populate('userId')
+      .then((result) => {
+        res.status(200).json(result)
+      })
+      .catch(error => next(error));
+  },
 };
 
 module.exports = rankingController;
+       
--- routes/ranking.js
+++ routes/ranking.js
@@ -3,6 +3,7 @@ const middlewares = require('../helpers/middlewares');
 
 const rankingRouter = (app) => {
   app.post('/api/ranking', middlewares.isCorrectToken(), rankingController.addRanking);
+  app.get('/api/ranking', rankingController.getRanking);
 };
 
 module.exports = rankingRouter;
