2b15748fa1d4f036452c43edf2f32416c18e6593
luis-sanfelices <l.fernando.sanfelices@gmail.com>
2018-05-06 13:33:21 +0200

    new query

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ const userRouter = (app) => {
   app.get('/api/user/:idUser', middlewares.isCorrectToken(), userController.searchUserById);
   app.put('/api/user/:idUser', middlewares.isCorrectToken(), userController.updateUser);
   app.put('/api/user/:idUser/avatar', middlewares.isCorrectToken(), upload.single('file'), userController.uploadAvatar);
+  app.get('/api/user/:idUser/lastQuizes', middlewares.isCorrectToken(), userController.getLastQuizesPlayed);
   app.get('/api/user/:idUser/friends', middlewares.isCorrectToken(), userController.getUserFriends);
   app.put('/api/user/:idUser/friends', middlewares.isCorrectToken(), userController.addFriend);
   app.delete('/api/user/:idUser/friends/:idFriend', middlewares.isCorrectToken(), userController.deleteFriend);
